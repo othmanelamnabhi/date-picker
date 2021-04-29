9d64bda2edaa53bade41e96ddeadbc2f0c1793a7
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2021-04-29 02:32:53 +0000

    finished the assignment, date-picker is working perfectly aside from a litle bug
    there seems to be an issue with the .date-picker-other-month-date class, when the page loads, it's assigned correctly to days outside of the month, but once I click on next or previous month, it doesn't behave as expected.
    I'll investigate that one tomorrow. (line 134)

--- script.js
+++ script.js
@@ -7,56 +7,137 @@ import {
   eachDayOfInterval,
   addMonths,
   subMonths,
+  parse,
   getMonth,
 } from "date-fns";
 
-// Select the date picker button and add an event listener to it
+// Select some HTML elements we'll use
 const dateButton = document.querySelector(".date-picker-button");
-dateButton.addEventListener("click", showDateModal);
+const currentMonth = document.querySelector(".current-month");
+const calendarDaysContainer = document.querySelector(".date-picker-grid-dates");
 
-function showDateModal(e) {
+// Show or hide the modal when the button is clicked
+dateButton.addEventListener("click", showHideDateModal);
+function showHideDateModal(e) {
   const dateModal = document.querySelector(".date-picker");
   dateModal.classList.toggle("show");
 }
 
-// When page loads, the button shows today's date
+// Fetch today's date
 const today = new Date();
-dateButton.innerText = format(today, "LLLL do, yyyy");
+
+// When page loads, the button shows today's date
+setButtonDate(today);
+// When page loads and the modal is opened, it shows the current month
+setCurrentMonth(today);
+calendarDaysContainer.innerHTML = renderDates(today);
+
+// When modal is open, the date on the button needs to also be selected on calendar
+const initiallySelectedDate = document.querySelector(
+  `[data-date="${dateButton.innerText}"]`
+);
+initiallySelectedDate.classList.add("selected");
+
+/* What happens when I click on the right arrow? */
+const nextArrow = document.querySelector(".next-month-button");
+nextArrow.addEventListener("click", () => {
+  const parsedDate = parse(currentMonth.innerText, "LLLL - yyyy", new Date());
+  const nextMonth = getNextMonth(parsedDate);
+  calendarDaysContainer.innerHTML = renderDates(nextMonth);
+  setCurrentMonth(nextMonth);
+});
+
+/* What happens when I click on the left arrow? */
+const prevArrow = document.querySelector(".prev-month-button");
+prevArrow.addEventListener("click", () => {
+  const parsedDate = parse(currentMonth.innerText, "LLLL - yyyy", new Date());
+  const prevMonth = getPreviousMonth(parsedDate);
+  calendarDaysContainer.innerHTML = renderDates(prevMonth);
+  setCurrentMonth(prevMonth);
+});
+
+/* What happens when I select a date in the calendar? */
+calendarDaysContainer.addEventListener("click", (e) => {
+  if (!e.target.matches(".date")) return;
+  const selectAllDates = document.querySelectorAll(".date");
+  selectAllDates.forEach((date) => {
+    date.classList.remove("selected");
+  });
+  e.target.classList.add("selected");
+  const selectedDate = parse(
+    e.target.dataset.date,
+    "LLLL do, yyyy",
+    new Date()
+  );
+  setButtonDate(selectedDate);
+});
+
+// Set date of the button
+function setButtonDate(selectedDate) {
+  dateButton.innerText = format(selectedDate, "LLLL do, yyyy");
+}
+
+// set month and year of the calendar displayed
+function setCurrentMonth(someDate) {
+  currentMonth.innerText = format(someDate, "LLLL - yyyy");
+}
 
 // Create variables to hold the value of previous and next month
-const nextMonth = (randomDate) => addMonths(randomDate, 1);
-const previousMonth = (randomDate) => subMonths(randomDate, 1);
-
-console.log(previousMonth(today), nextMonth(today));
+function getNextMonth(randomDate) {
+  return addMonths(randomDate, 1);
+}
+function getPreviousMonth(randomDate) {
+  return subMonths(randomDate, 1);
+}
 
 /* List all days of the month */
 // 1- Find 1st day of any month
-const whenMonthStarts = (randomDate) => startOfMonth(randomDate);
-
-console.log(whenMonthStarts(today));
+function whenMonthStarts(randomDate) {
+  return startOfMonth(randomDate);
+}
 
 // 2- Find last day of any month
-const whenMonthEnds = (randomDate) => endOfMonth(randomDate);
-
-console.log(whenMonthEnds(today));
+function whenMonthEnds(randomDate) {
+  return endOfMonth(randomDate);
+}
 
 // 3- find start of week for 1st day of the month
-const whenIsStartWeek = (whenMonthStartsFunction) =>
-  startOfWeek(whenMonthStartsFunction);
+function whenIsStartWeek(whenMonthStartsFunction) {
+  return startOfWeek(whenMonthStartsFunction);
+}
 
-console.log(whenIsStartWeek(whenMonthStarts(today)));
-
-// 4- find endofweek for 1st day
-const whenIsEndWeek = (whenMonthEndsFunction) =>
-  endOfWeek(whenMonthEndsFunction);
-
-console.log(whenIsEndWeek(whenMonthEnds(today)));
+// 4- find endofweek for last day of the month
+function whenIsEndWeek(whenMonthEndsFunction) {
+  return endOfWeek(whenMonthEndsFunction);
+}
 
 // 5- create array of date from interval
-const createArrayFromInterval = () =>
-  eachDayOfInterval({
-    start: whenIsStartWeek(whenMonthStarts(today)),
-    end: whenIsEndWeek(whenMonthEnds(today)),
+function createArrayFromInterval(date) {
+  return eachDayOfInterval({
+    start: whenIsStartWeek(whenMonthStarts(date)),
+    end: whenIsEndWeek(whenMonthEnds(date)),
   });
+}
 
-console.log(createArrayFromInterval());
+/* Render interval of dates */
+// map the array / create div / add classes / insert the day / add the attribute
+function renderDates(date) {
+  return createArrayFromInterval(date)
+    .map((date) => {
+      const dateButt = document.createElement("button");
+      const parsedDate = parse(
+        currentMonth.innerText,
+        "LLLL - yyyy",
+        new Date()
+      );
+      dateButt.classList.add("date");
+      if (getMonth(date) !== getMonth(parsedDate))
+        dateButt.classList.add("date-picker-other-month-date");
+      dateButt.dataset.date = format(date, "LLLL do, yyyy");
+      dateButt.innerText = format(date, "d");
+      return dateButt.outerHTML;
+    })
+    .reduce((sum, current) => {
+      return sum + current;
+    });
+}
